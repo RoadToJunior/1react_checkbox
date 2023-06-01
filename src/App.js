19b763ee5bb7aa7873b16688b0da1cc031808640
NowWeAre-Free <kontodarm@wp.pl>
2023-06-01 20:11:09 +0200

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    text: "Nie możesz obejrzeć tego filmu jeśli masz mniej niż 16 lat!",
+  };
+
+  handleChange = () => {};
+
+  render() {
+    return (
+      <>
+        <h1>Kup bilet na horror roku!</h1>
+        <input type="checkbox" onChange={this.handleChange} id="input" />
+        <label htmlFor="input">Mam co najmnie 16 lat</label>
+
+        <p>{this.state.text}</p>
+      </>
+    );
+  }
 }
 
 export default App;
