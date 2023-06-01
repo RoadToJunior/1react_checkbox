c0474b5ebb675b97b354edcfba6286184a78d31d
NowWeAre-Free <kontodarm@wp.pl>
2023-06-01 22:13:05 +0200

    add display message connected to checkbox

--- src/App.js
+++ src/App.js
@@ -12,12 +12,26 @@ class CheckboxAgeConfirmation extends React.Component {
     });
   };
 
+  displayMessage = () => {
+    if (this.state.isConfirmed) {
+      return <PositiveMessage />;
+    } else {
+      return <NegativeMessage />;
+    }
+  };
+
   render() {
     return (
       <>
         <h1>Kup bilet na horror roku!</h1>
-        <input type="checkbox" id="age" onChange={this.handleChange} />
+        <input
+          type="checkbox"
+          id="age"
+          onChange={this.handleChange}
+          checked={this.state.isConfirmed}
+        />
         <label htmlFor="age">Mam co najmnie 16 lat</label>
+        {this.displayMessage()}
       </>
     );
   }
